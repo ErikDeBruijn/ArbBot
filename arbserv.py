6d1c2f06dcfb4dfd22b79bf23770fbca55ad65da
Erik de Bruijn <github@erikdebruijn.nl>
2018-01-14 20:57:54 +0100

    made arbitrage webserver automatically reload the dataframe.

--- arbserv.py
+++ arbserv.py
@@ -7,7 +7,9 @@ import plotly.graph_objs as go
 import plotly.plotly as py
 import plotly.figure_factory as ff
 
+import socket
 import datetime, time
+
 
 def to_unix_time(dt):
     epoch =  datetime.datetime.utcfromtimestamp(0)
@@ -28,131 +30,141 @@ import pandas as pd
 # 2    0.00198      -3.46%       1.34%     NaN
 # 3    0.00198      -3.41%       1.24%     NaN
 # 4    0.00198      -3.26%       1.24%     NaN
-skipfirst = 1500
-skiprows = range(1,skipfirst+1)
-cols = ['time','bitgrail_sell','bitgrail_buy','kcoin_sell','kcoin_buy','traded']
-df = pd.read_csv("log.csv",delimiter='\t',usecols=cols,header=0,parse_dates=[0],skiprows=skiprows)
-# dfBG = pd.read_csv("log.csv",delimiter='\t',usecols=['time','bitgrail_sell'],header=0,parse_dates=[0],skiprows=skiprows)
-# dfKC = pd.read_csv("log.csv",delimiter='\t',usecols=['time',],header=0,parse_dates=[0],skiprows=skiprows)
+def getLayout():
+    print "===== gathering latest data ====="
+    skipfirst = 1500
+    skiprows = range(1,skipfirst+1)
+    cols = ['time','bitgrail_sell','bitgrail_buy','kcoin_sell','kcoin_buy','traded']
+    df = pd.read_csv("log.csv",delimiter='\t',usecols=cols,header=0,parse_dates=[0],skiprows=skiprows)
+    # dfBG = pd.read_csv("log.csv",delimiter='\t',usecols=['time','bitgrail_sell'],header=0,parse_dates=[0],skiprows=skiprows)
+    # dfKC = pd.read_csv("log.csv",delimiter='\t',usecols=['time',],header=0,parse_dates=[0],skiprows=skiprows)
 
-# df = pd.read_csv(
-#     'https://gist.githubusercontent.com/chriddyp/' +
-#     '5d1ea79569ed194d432e56108a04d188/raw/' +
-#     'a9f9e8076b837d541398e999dcbac2b2826a81f8/'+
-#     'gdp-life-exp-2007.csv')
+    # df = pd.read_csv(
+    #     'https://gist.githubusercontent.com/chriddyp/' +
+    #     '5d1ea79569ed194d432e56108a04d188/raw/' +
+    #     'a9f9e8076b837d541398e999dcbac2b2826a81f8/'+
+    #     'gdp-life-exp-2007.csv')
 
-print(df.head())
+    print(df.head())
 
-# exit()
-# table = ff.create_table(df)
+    # exit()
+    # table = ff.create_table(df)
 
+    trace = {}
+    trace['BG_sell'] = go.Scatter(
+        x = df['time'],
+        y = df['bitgrail_sell'],
+        yaxis = 'y',
+        name = 'bitgrail_sell',
+        line = dict(
+            color = ('rgb(6, 102, 12)'),
+            width = 1)
+    )
+    trace['BG_buy'] = go.Scatter(
+        x = df['time'],
+        y = df['bitgrail_buy'],
+        yaxis = 'y',
+        name = 'bitgrail_buy',
+        line = dict(
+            color = ('rgb(12, 205, 24)'),
+            width = 1)
+    )
+    trace['KC_sell'] = go.Scatter(
+        x = df['time'],
+        y = df['kcoin_sell'],
+        yaxis = 'y',
+        name = 'kcoin_sell',
+        line = dict(
+            color = ('rgb(22, 96, 167)'),
+            width = 1)
+    )
+    trace['KC_buy'] = go.Scatter(
+        x = df['time'],
+        y = df['kcoin_buy'],
+        yaxis = 'y',
+        name = 'kcoin_buy',
+        line = dict(
+            color = ('rgb(31, 134, 233)'),
+            width = 1)
+    )
+    trace['traded'] = go.Bar(
+        x = df['time'],
+        y = df['traded'],
+        yaxis = 'y2',
+        name = 'traded',
+        # color = ('rgb(255, 145, 0)'),
+        width = 0.4
+    )
 
-app = dash.Dash()
-trace = {}
-trace['BG_sell'] = go.Scatter(
-    x = df['time'],
-    y = df['bitgrail_sell'],
-    yaxis = 'y',
-    name = 'bitgrail_sell',
-    line = dict(
-        color = ('rgb(6, 102, 12)'),
-        width = 1)
-)
-trace['BG_buy'] = go.Scatter(
-    x = df['time'],
-    y = df['bitgrail_buy'],
-    yaxis = 'y',
-    name = 'bitgrail_buy',
-    line = dict(
-        color = ('rgb(12, 205, 24)'),
-        width = 1)
-)
-trace['KC_sell'] = go.Scatter(
-    x = df['time'],
-    y = df['kcoin_sell'],
-    yaxis = 'y',
-    name = 'kcoin_sell',
-    line = dict(
-        color = ('rgb(22, 96, 167)'),
-        width = 1)
-)
-trace['KC_buy'] = go.Scatter(
-    x = df['time'],
-    y = df['kcoin_buy'],
-    yaxis = 'y',
-    name = 'kcoin_buy',
-    line = dict(
-        color = ('rgb(31, 134, 233)'),
-        width = 1)
-)
-trace['traded'] = go.Bar(
-    x = df['time'],
-    y = df['traded'],
-    yaxis = 'y2',
-    name = 'traded',
-    # color = ('rgb(255, 145, 0)'),
-    width = 0.4
-)
+    data = [trace['BG_sell'], trace['BG_buy'], trace['KC_sell'], trace['KC_buy']]
+    # data = [trace['BG_sell'], trace['BG_buy'], trace['KC_sell'], trace['KC_buy'], trace['traded']]
 
-data = [trace['BG_sell'], trace['BG_buy'], trace['KC_sell'], trace['KC_buy']]
-# data = [trace['BG_sell'], trace['BG_buy'], trace['KC_sell'], trace['KC_buy'], trace['traded']]
-config = {'scrollZoom': True}
+    # Edit the layout
+    layout = dict(title = 'Prices of exchanges',
+                  xaxis = dict(
+                    title = 'Time',
+                    range = [1000*time.time()-60*60*24000,1000*time.time()],
+                    rangeselector=dict(
+                    buttons=list([
+                        dict(count=1,
+                             label='1h',
+                             step='hour',
+                             stepmode='backward'),
+                        dict(count=6,
+                             label='6h',
+                             step='hour',
+                             stepmode='backward'),
+                        dict(count=24,
+                             label='24h',
+                             step='hour',
+                             stepmode='backward'),
+                        dict(count=3,
+                             label='3d',
+                             step='day',
+                             stepmode='backward'),
+                                dict(step='all')
+                                ])
+                    ),
+                    
+                    type='date'),
+                  yaxis = dict(title = 'Price (BTC)'),
+                  yaxis2 = dict(title = 'Revenue (BTC)', side='right',overlaying='y')
+                  
+                  )
 
-# Edit the layout
-layout = dict(title = 'Prices of exchanges',
-              xaxis = dict(
-                title = 'Time',
-                range = [1000*time.time()-60*60*24000,1000*time.time()],
-                rangeselector=dict(
-                buttons=list([
-                    dict(count=1,
-                         label='1h',
-                         step='hour',
-                         stepmode='backward'),
-                    dict(count=6,
-                         label='6h',
-                         step='hour',
-                         stepmode='backward'),
-                    dict(count=24,
-                         label='24h',
-                         step='hour',
-                         stepmode='backward'),
-                    dict(count=3,
-                         label='3d',
-                         step='day',
-                         stepmode='backward'),
-                            dict(step='all')
-                            ])
-                ),
-                
-                type='date'),
-              yaxis = dict(title = 'Price (BTC)'),
-              yaxis2 = dict(title = 'Revenue (BTC)', side='right',overlaying='y')
-              
-              )
+    layout['annotations'] = [
+        dict(xref='paper', yref='paper', x=0.95, y=.5,
+              xanchor='center', yanchor='top',
+              text='Diff: ',
+              font=dict(family='Arial',
+                        size=10,
+                        color='rgb(150,150,150)'),
+              showarrow=False)
+    ]
 
-layout['annotations'] = [
-    dict(xref='paper', yref='paper', x=0.95, y=.5,
-          xanchor='center', yanchor='top',
-          text='Diff: ',
-          font=dict(family='Arial',
-                    size=10,
-                    color='rgb(150,150,150)'),
-          showarrow=False)
-]
-
-fig = dict(data=data, layout=layout, config=config)
-
-app.layout = html.Div(children=[
-    html.Div(children='''
-        See below graph for price information.
-    '''),
-
-    dcc.Graph(
+    config = {'scrollZoom': True}
+    fig = dict(data=data, layout=layout)
+    graphObject = dcc.Graph(
         id='prices',
         figure=fig,config=config
     )
-])
+    layout = html.Div(children=[
+    html.Div(children='Last data fetch: ' + str(datetime.datetime.now())),
+
+    graphObject
+    ])
+    return layout
+
+
+app = dash.Dash()
+
+# fig = createFig()
+
+
+app.layout = getLayout
+
+PORT = 8050
+ADDRESS = '0.0.0.0'
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(port=PORT, host=ADDRESS, debug=True)
